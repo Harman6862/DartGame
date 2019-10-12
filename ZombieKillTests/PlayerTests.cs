71c4c5c7f953fd8762855f945c6d14a479f53174
ankits <ankits@ANKITSPF17TXHW>
2019-10-12 20:55:15 +0530

    all test

--- ZombieKillTests/PlayerTests.cs
+++ ZombieKillTests/PlayerTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ZombieKill;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ZombieKill.Tests
 {
