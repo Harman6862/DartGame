cc8678408080c71c24cf08128886ea44e0d04975
ankits <ankits@ANKITSPF17TXHW>
2019-10-12 20:31:39 +0530

    commite all tests

---
+++ ZombieKillTests/PlayerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZombieKill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieKill.Tests
+{
+    [TestClass()]
+    public class PlayerTests
+    {
+        public void ThrowKnifeTest()
+        {
+            Player player = new Player();
+
+            Boolean knifePosition = player.SetKnife(6);
+            Boolean luckValue = player.TryLuck(-1);
+            player.ThrowKnife();
+            Assert.IsTrue(knifePosition == false, "Random number not between 0 - 5");
+            Assert.IsTrue(luckValue == false, "Random number not between 0 - 5");
+        }
+    }
+}
