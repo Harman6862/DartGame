b74704a921c8d2c1f23b5504c0efbad0edc3e9e6
ankits <ankits@ANKITSPF17TXHW>
2019-10-12 16:27:42 +0530

    tests

---
+++ GhostBowlingTests/PlayerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GhostBowling.Tests
+{
+    [TestClass()]
+    public class PlayerTests
+    {
+        [TestMethod()]
+        public void BowlTest()
+        {
+            Player player = new Player();
+
+            Boolean ballPosition = player.SetTheBall(6);
+            Boolean luckValue = player.TryLuck(-1);
+            player.Bowl();
+            Assert.IsTrue(ballPosition == false, "Random number not between 0 - 5");
+            Assert.IsTrue(luckValue == false, "Random number not between 0 - 5");
+        }
+    }
+}
